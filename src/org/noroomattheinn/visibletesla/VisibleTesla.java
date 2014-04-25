dad02e91f4e8ee09b1ecc9ef23ec01c3b4ae925f
Joe Pasqua <joe@NoRoomAtTheInn.org>
2014-04-25 16:50:26 -0700

    Clean up imports

--- src/org/noroomattheinn/visibletesla/VisibleTesla.java
+++ src/org/noroomattheinn/visibletesla/VisibleTesla.java
@@ -8,7 +8,6 @@ package org.noroomattheinn.visibletesla;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
