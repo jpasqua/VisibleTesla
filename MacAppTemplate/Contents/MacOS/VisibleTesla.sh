c642ba7f418f05260e4638869aaeea42987b0825
Joe Pasqua <joe@NoRoomAtTheInn.org>
2014-04-26 00:51:45 -0700

    Add template for mac app bundle

---
+++ MacAppTemplate/Contents/MacOS/VisibleTesla.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PRG=$0
+
+while [ -h "$PRG" ]; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '^.*-> \(.*\)$' 2>/dev/null`
+    if expr "$link" : '^/' 2> /dev/null >/dev/null; then
+        PRG="$link"
+    else
+        PRG="`dirname "$PRG"`/$link"
+    fi
+done
+
+progdir=`dirname "$PRG"`
+
+if [ -n "$JAVA_HOME" ]; then
+  JAVACMD="$JAVA_HOME/bin/java"
+elif [ -x /usr/libexec/java_home ]; then
+  JAVACMD="`/usr/libexec/java_home`/bin/java"
+else
+  JAVACMD="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
+fi
+
+VM_PARAMS=""
+VM_PARAMS_FILE=$progdir/vmparams.txt
+if [ -f $VM_PARAMS_FILE ]; then
+  VM_PARAMS=" `cat $VM_PARAMS_FILE` "
+fi
+cd $progdir/../../..
+exec $JAVACMD -jar $VM_PARAMS -Xdock:icon=FobIcon.png VisibleTesla.jar
