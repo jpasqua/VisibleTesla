e1b87f6a5fab72554c6fd3bac11dbbd07b0bc91d
Joe Pasqua <joe@NoRoomAtTheInn.org>
2014-07-05 23:02:10 -0700

    Update build and support files

--- InstallSupport/Windows/vtrunner.bat
+++ InstallSupport/Windows/vtrunner.bat
@@ -1,1 +1,10 @@
-start javaw -Xmx1000m -jar VisibleTesla.jar
+REM
+REM Tests whether there's a locally installed Java and uses it
+REM
+
+	IF NOT EXIST .\jre1.7 GOTO NO_LOCAL_JAVA
+	start .\jre1.7\bin\javaw -Xmx1024m -jar VisibleTesla.jar
+	GOTO DONE
+:NO_LOCAL_JAVA
+	start javaw -Xmx1024m -jar VisibleTesla.jar
+:DONE
